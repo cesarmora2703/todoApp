1706bd6f1e13153de2eb7650d9244a8d33d63de9
cesarmora2703 <cesarmora2703@gmail.com>
2023-04-13 12:33:33 -0500

    Initial refactor

--- main.py
+++ main.py
@@ -1,55 +1,52 @@
-from flask import Flask, make_response, redirect, request, render_template,session, url_for, flash
+from flask import make_response, redirect, request, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
 import unittest
+from app import create_app
+from app.forms import LoginForm
 
-app = Flask(__name__)
-Bootstrap(app)
+app = create_app()
 
-app.config['SECRET_KEY'] = 'SUPER SECRETO'
+todos = ['Comprar café', 'Enviar solicitud de compra',
+         'Entregar video a productor']
 
-todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video a productor']
-
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators = [DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
 
 @app.cli.command()
 def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner().run(tests)
 
+
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', error=error)
 
+
 @app.errorhandler(500)
 def internal_server_error(error):
     return render_template('500.html', error=error), 500
+
 
 @app.route('/')
 def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    #response.set_cookie('user_ip', user_ip)
+    # response.set_cookie('user_ip', user_ip)
     session['user_ip'] = user_ip
 
     return response
 
-@app.route('/hello', methods = ['GET', 'POST'])
+
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
-    #user_ip = request.cookies.get('user_ip')
+    # user_ip = request.cookies.get('user_ip')
     user_ip = session.get('user_ip')
     login_form = LoginForm()
     username = session.get('username')
 
     context = {
-        'user_ip' : user_ip,
-        'todos' : todos,
+        'user_ip': user_ip,
+        'todos': todos,
         'login_form': login_form,
         'username': username,
     }
